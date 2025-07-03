6aeb8e51dd1eeb880d3f40fb24a4d4932fc0026c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-03 02:54:13 -0700

    Tagged Tag Tags
    Yes. This makes sense now, I promise.
    
    So the thing is that there has to be a type that represents 'all' NBT types. That's what `Tag` is here. `TagLike` is what 'tags' whether the type is a valid type itself. So this is how the recursive part works, in reference to how NBTify handles things over in TypeScript land. I'm pretty sure this seemingly fixes the dilemma between things like CompoundTag and CompoundTagLike for example. Weird indexing issues. Yet to see though, of course!

--- src/nbt/tag.rs
+++ src/nbt/tag.rs
@@ -1,5 +1,22 @@
 use std::collections::HashMap;
 
+pub enum Tag {
+    Byte(ByteTag),
+    Short(ShortTag),
+    Int(IntTag),
+    Long(LongTag),
+    Float(FloatTag),
+    Double(DoubleTag),
+    ByteArray(ByteArrayTag),
+    String(StringTag),
+    List(ListTag<Tag>),
+    Compound(CompoundTag),
+    IntArray(IntArrayTag),
+    LongArray(LongArrayTag),
+}
+
+impl TagLike for Tag {}
+
 pub struct ByteTag(i8);
 pub struct ShortTag(i16);
 pub struct IntTag(i32);
@@ -8,22 +25,22 @@ pub struct FloatTag(f32);
 pub struct DoubleTag(f64);
 pub struct ByteArrayTag(Vec<i8>);
 pub struct StringTag(String);
-pub struct ListTag<T: Tag>(Vec<T>);
+pub struct ListTag<T: TagLike>(Vec<T>);
 pub struct CompoundTag(HashMap<String, Tag>);
 pub struct IntArrayTag(Vec<i32>);
 pub struct LongArrayTag(Vec<i64>);
 
-pub trait Tag {}
+pub trait TagLike {}
 
-impl Tag for ByteTag {}
-impl Tag for ShortTag {}
-impl Tag for IntTag {}
-impl Tag for LongTag {}
-impl Tag for FloatTag {}
-impl Tag for DoubleTag {}
-impl Tag for ByteArrayTag {}
-impl Tag for StringTag {}
-impl<T: Tag> Tag for ListTag<T> {}
-impl Tag for CompoundTag {}
-impl Tag for IntArrayTag {}
-impl Tag for LongArrayTag {}
+impl TagLike for ByteTag {}
+impl TagLike for ShortTag {}
+impl TagLike for IntTag {}
+impl TagLike for LongTag {}
+impl TagLike for FloatTag {}
+impl TagLike for DoubleTag {}
+impl TagLike for ByteArrayTag {}
+impl TagLike for StringTag {}
+impl<T: TagLike> TagLike for ListTag<T> {}
+impl TagLike for CompoundTag {}
+impl TagLike for IntArrayTag {}
+impl TagLike for LongArrayTag {}
