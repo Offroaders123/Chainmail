4e7de47ff0afb3ad67b658caf242923c489db21a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-05 01:54:12 -0700

    LCE Entities & Friends
    Okay, ported more of these over. I am getting heavily sleepy though, and I don't want to mess up this work for this project. I will see you next time around!

--- src/legacy_console/block_entity.rs
+++ src/legacy_console/block_entity.rs
@@ -1,60 +1,68 @@
-export type BlockEntity<K extends keyof BlockEntityNameMap = keyof BlockEntityNameMap> = BlockEntityNameMap[K];
+// export type BlockEntity<K extends keyof BlockEntityNameMap = keyof BlockEntityNameMap> = BlockEntityNameMap[K];
 
-export type BlockEntityNameMap = {
-  // temp, also unsure of all of the values here yet
-  [K in BlockEntityResource]: BlockEntityLike<K>;
-};
+// export type BlockEntityNameMap = {
+//   // temp, also unsure of all of the values here yet
+//   [K in BlockEntityResource]: BlockEntityLike<K>;
+// };
 
-export interface BlockEntityLike<BlockEntityID extends string> {
-  id: `${BlockEntityID}`;
+use serde::{Deserialize, Serialize};
+
+use crate::nbt::tag::StringTag;
+
+// <BlockEntityID extends string>
+#[derive(Serialize, Deserialize)]
+pub struct BlockEntityLike {
+    id: StringTag, // should this be `StringTag<BlockEntityResource>`, and or generic?
 }
 
-export enum BlockEntityResource {
-  Airportal = "Airportal",
-  Banner = "Banner",
-  Beacon = "Beacon",
-  Cauldron = "Cauldron",
-  Chest = "Chest",
-  Comparator = "Comparator",
-  DLDetector = "DLDetector",
-  Dropper = "Dropper",
-  EnchantTable = "EnchantTable",
-  EndGateway = "EndGateway",
-  EnderChest = "EnderChest",
-  FlowerPot = "FlowerPot",
-  Furnace = "Furnace",
-  Hopper = "Hopper",
-  MobSpawner = "MobSpawner",
-  Music = "Music",
-  Piston = "Piston",
-  RecordPlayer = "RecordPlayer",
-  Sign = "Sign",
-  Skull = "Skull",
-  Trap = "Trap",
-  banner = "banner",
-  beacon = "beacon",
-  bed = "bed",
-  brewing_stand = "brewing_stand",
-  cauldron = "cauldron",
-  chest = "chest",
-  comparator = "comparator",
-  conduit = "conduit",
-  daylight_detector = "daylight_detector",
-  dispenser = "dispenser",
-  dropper = "dropper",
-  enchanting_table = "enchanting_table",
-  end_gateway = "end_gateway",
-  end_portal = "end_portal",
-  ender_Chest = "ender_Chest",
-  ender_chest = "ender_chest",
-  flower_pot = "flower_pot",
-  furnace = "furnace",
-  hopper = "hopper",
-  jukebox = "jukebox",
-  mob_spawner = "mob_spawner",
-  noteblock = "noteblock",
-  piston = "piston",
-  shulker_box = "shulker_box",
-  sign = "sign",
-  skull = "skull"
+#[allow(non_camel_case_types)]
+#[derive(Serialize, Deserialize)]
+pub enum BlockEntityResource {
+    Airportal,
+    Banner,
+    Beacon,
+    Cauldron,
+    Chest,
+    Comparator,
+    DLDetector,
+    Dropper,
+    EnchantTable,
+    EndGateway,
+    EnderChest,
+    FlowerPot,
+    Furnace,
+    Hopper,
+    MobSpawner,
+    Music,
+    Piston,
+    RecordPlayer,
+    Sign,
+    Skull,
+    Trap,
+    banner,
+    beacon,
+    bed,
+    brewing_stand,
+    cauldron,
+    chest,
+    comparator,
+    conduit,
+    daylight_detector,
+    dispenser,
+    dropper,
+    enchanting_table,
+    end_gateway,
+    end_portal,
+    ender_Chest,
+    ender_chest,
+    flower_pot,
+    furnace,
+    hopper,
+    jukebox,
+    mob_spawner,
+    noteblock,
+    piston,
+    shulker_box,
+    sign,
+    skull,
 }
