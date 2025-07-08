f73216c3b61564d7c69af2775ac4aa7d4dbae945
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-07 22:59:36 -0700

    Convert Module Namespaces
    Okay, I think this is the way to go? We'll see. It's too much to import things with similar names into named imports. It would involve WAY too many aliases.

--- src/convert/java/v1_20/level.rs
+++ src/convert/java/v1_20/level.rs
@@ -1,10 +1,8 @@
 use crate::{
     convert::convert_to::ConvertTo,
-    schema::{
-        java::v1_20::level::LevelDat as JavaLevelDat, pi::v0_1_1::level::LevelDat as PiLevelDat,
-    },
+    schema::{java::v1_20::level as java, pi::v0_1_1::level as pi},
 };
 
-impl ConvertTo<PiLevelDat> for JavaLevelDat {
-    fn convert(&self) -> PiLevelDat {}
+impl ConvertTo<pi::LevelDat> for java::LevelDat {
+    fn convert(&self) -> pi::LevelDat {}
 }
--- src/convert/pi/v0_1_1/level.rs
+++ src/convert/pi/v0_1_1/level.rs
@@ -1,10 +1,8 @@
 use crate::{
     convert::convert_to::ConvertTo,
-    schema::{
-        java::v1_20::level::LevelDat as JavaLevelDat, pi::v0_1_1::level::LevelDat as PiLevelDat,
-    },
+    schema::{java::v1_20::level as java, pi::v0_1_1::level as pi},
 };
 
-impl ConvertTo<JavaLevelDat> for PiLevelDat {
-    fn convert(&self) -> JavaLevelDat {}
+impl ConvertTo<java::LevelDat> for pi::LevelDat {
+    fn convert(&self) -> java::LevelDat {}
 }
