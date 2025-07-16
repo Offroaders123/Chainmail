f455910bb8393486c05bf82907a8b9900a159687
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-15 23:50:38 -0700

    Pi Level -> Java (More Progress)
    Coming back to the project after a few days, have to get my mind around how this works once again. More in just how I had organized things, the goal is to not repeat myself where it isn't necessary.
    
    Please listen to Mike Keneally "Li'l (Signature Sound version)", I absolutely love that song. It is very high up there with "Cornbread Crumb" for me. Wow, I absolutely love his music. It's just so darn tangible! The room feel of it all, and that it's in the moment. It's liberating.

--- src/convert/pi/v0_1_1/level.rs
+++ src/convert/pi/v0_1_1/level.rs
@@ -5,6 +5,61 @@ use crate::{
 
 impl ConvertTo<java::LevelDat> for pi::LevelDat {
     fn convert(&self) -> java::LevelDat {
-        java::LevelDat {}
+        java::LevelDat {
+            Data: self.convert(),
+        }
+    }
+}
+
+impl ConvertTo<java::LevelDatData> for pi::LevelDat {
+    fn convert(&self) -> java::LevelDatData {
+        java::LevelDatData {
+            allowCommands: self.allowCommands,
+            BorderCenterX: self.BorderCenterX,
+            BorderCenterY: self.BorderCenterY,
+            BorderDamagePerBlock: self.BorderDamagePerBlock,
+            BorderSize: self.BorderSize,
+            BorderSafeZone: self.BorderSafeZone,
+            BorderSizeLerpTarget: self.BorderSizeLerpTarget,
+            BorderSizeLerpTime: self.BorderSizeLerpTime,
+            BorderWarningBlocks: self.BorderWarningBlocks,
+            BorderWarningTime: self.BorderWarningTime,
+            clearWeatherTime: self.clearWeatherTime,
+            CustomBossEvents: self.CustomBossEvents,
+            DataPacks: self.DataPacks,
+            DataVersion: self.DataVersion,
+            DayTime: self.DayTime,
+            Difficulty: self.Difficulty,
+            DifficultyLocked: self.DifficultyLocked,
+            DimensionData: self.DimensionData,
+            GameRules: self.GameRules,
+            WorldGenSettings: self.WorldGenSettings,
+            GameType: self.GameType.convert(),
+            generatorName: self.generatorName,
+            generatorOptions: self.generatorOptions,
+            generatorVersion: self.generatorVersion,
+            hardcore: self.hardcore,
+            initialized: self.initialized,
+            LastPlayed: self.LastPlayed,
+            LevelName: self.LevelName,
+            MapFeatures: self.MapFeatures,
+            Player: self.Player.convert(),
+            raining: self.raining,
+            rainTime: self.rainTime,
+            RandomSeed: self.RandomSeed,
+            SizeOnDisk: self.SizeOnDisk,
+            SpawnX: self.SpawnX,
+            SpawnY: self.SpawnY,
+            SpawnZ: self.SpawnZ,
+            thundering: self.thundering,
+            thunderTime: self.thunderTime,
+            Time: self.Time,
+            version: self.version,
+            Version: self.Version,
+            WanderingTraderId: self.WanderingTraderId,
+            WanderingTraderSpawnChance: self.WanderingTraderSpawnChance,
+            WanderingTraderSpawnDelay: self.WanderingTraderSpawnDelay,
+            WasModded: self.WasModded,
+        }
     }
 }
